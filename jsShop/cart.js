9e7970669d3d08e6275c03d19d259a20cc037d23
vankokos <vano9995@gmail.com>
2021-02-08 01:53:04 +0200

    circle

--- jsShop/cart.js
+++ jsShop/cart.js
@@ -13,23 +13,22 @@ btn.addEventListener("click", function () {
 
 
 var btnAdd = document.querySelectorAll(".main__inner-grid-item-btn")
-
-var cartButtons = document.querySelectorAll("header__inner-menu-item-cart-content-bottom")
+var cartButtons = document.querySelectorAll(".header__inner-menu-item-cart-content-bottom button")
 
 for (let i = 0; i < btnAdd.length; i++) {
     btnAdd[i].addEventListener("click", function(e) {
         var div = e.target.closest("div")
         console.log(div)
-
+        
         var img = div.querySelector(".main__inner-grid-item-img")
         var price = div.querySelector(".main__inner-grid-item-price")
-
+        
         console.log(img.src)
         console.log(price.innerHTML)
-
+        
         var cartItem = document.createElement("div")
         cartItem.classList.add("header__inner-menu-item-cart-content-item")
-
+        
         var close = document.createElement("img")
         close.classList.add("header__inner-menu-item-cart-content-item-delete")
         close.src = "img/cancel.svg"
@@ -37,23 +36,42 @@ for (let i = 0; i < btnAdd.length; i++) {
             var targetDiv = e.target.closest("div")
             content.removeChild(targetDiv)
         })
-
+        
         var shoeImg = document.createElement("img")
         shoeImg.classList.add("header__inner-menu-item-cart-content-item-shoe")
         shoeImg.src = img.src
-
+        
         var shoePrice = document.createElement("p")
         shoePrice.classList.add("header__inner-menu-item-cart-content-item-price")
         shoePrice.innerHTML = price.innerHTML
+        
+        var cartPop = document.querySelector(".header__inner-menu-item-cart-pop")
+        console.log(cartPop)
+        var count;
 
 
-
+        setTimeout(() => {
+            cartPop.classList.add("active")
+            cartPop.classList.remove("unactive")
+            setTimeout(() => {
+                cartPop.classList.remove("active")
+                cartPop.classList.add("unactive")        
+            }, 1000);
+        }, 100);
+        
         content.appendChild(cartItem)
         cartItem.appendChild(close)
         cartItem.appendChild(shoeImg)
         cartItem.appendChild(shoePrice)
-
-
+        
+        for (let i = 0; i < cartButtons.length; i++) {
+            cartButtons[i].addEventListener("click", function(e) {
+                if(e.target.id == "del") {
+                    content.removeChild(cartItem)
+                }
+            })  
+        }
     })
     
 }
+
